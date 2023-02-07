76845df0991d08727672663a8b1b89bfe240b616
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2023-02-07 12:42:42 +0600

    server connected on mongodb

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// DB syntax
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wbb4jrx.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+
+
+// routes
+app.get('/', (req, res) => {
+    res.send('ema john server is running');
+})
+
+
+
+
+
+// at last
+app.listen(port, () => {
+    console.log(`ema john simple running on: ${port}`)
+})
